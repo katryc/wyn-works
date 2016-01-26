14f75e30dc950c65df6305fa10b8969066bb524c
katryc <katryc@outlook.com>
2016-01-26 11:30:38 -0500

    Update audition.rb

--- audition.rb
+++ audition.rb
@@ -1,27 +1,30 @@
-# CHEETAH_GIRLS = [
-#   "Raven-Symone",
-#   "Adrienne Bailon",
-#   "Sabrina Bryan",
-#   "Kiely Williams"
-# ]
-#
-# auditions = [
-#   "Raven-Symone",
-#   "Usher",
-#   "Wiz Khalifa",
-#   "Adrienne Bailon",
-#   "Hulk Hogan",
-#   "Sabrina Bryan",
-#   "Diego Lugo",
-#   "Kiely Williams",
-#   "Justin Timberlake"
-# ]
-#
-# panel = auditions << CHEETAH_GIRLS
-#
-#
-# puts panel.slice!(9,10)
+CHEETAH_GIRLS = [
+  "Raven-Symone",
+  "Adrienne Bailon",
+  "Sabrina Bryan",
+  "Kiely Williams"
+]
 
+auditions = [
+  "Raven-Symone",
+  "Usher",
+  "Wiz Khalifa",
+  "Adrienne Bailon",
+  "Hulk Hogan",
+  "Sabrina Bryan",
+  "Diego Lugo",
+  "Kiely Williams",
+  "Justin Timberlake"
+]
+
+panel = auditions << CHEETAH_GIRLS
+
+
+puts panel.slice!(9,10)
+
+#_______________________________________________________
+
+##Ex. 2
 # Iterate thru names
 def iterate_thru_pets
 pets = ["Scooby", "Soco", "Summer", "Pixie", "Wilson", "Mason","Baron", "Brinkley", "Bella"].each do |name|
@@ -34,10 +37,10 @@ pets = ["Scooby", "Soco", "Summer", "Pixie", "Wilson", "Mason","Baron", "Brinkle
 end
 end
 iterate_thru_pets
-# def iterate_thru_pets
-# if first_letter == "s"
-# puts “My name starts with an S for super!”
-# else
-# puts “I’m still pretty special too!”
-# end
-# end
+def iterate_thru_pets
+if first_letter == "s"
+puts “My name starts with an S for super!”
+else
+puts “I’m still pretty special too!”
+end
+end
